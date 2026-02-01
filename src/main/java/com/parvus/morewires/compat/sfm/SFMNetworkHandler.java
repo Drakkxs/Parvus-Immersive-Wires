2657d98599e113222535b95e632d44565fe57120
DRAKKKS <70417194+Drakkxs@users.noreply.github.com>
2026-01-31 22:59:10 -0500

    Step towards trying to figure it out.

--- src/main/java/com/parvus/morewires/compat/sfm/SFMNetworkHandler.java
+++ src/main/java/com/parvus/morewires/compat/sfm/SFMNetworkHandler.java
@@ -3,55 +3,36 @@ package com.parvus.morewires.compat.sfm;
 import blusunrize.immersiveengineering.api.wires.GlobalWireNetwork;
 import blusunrize.immersiveengineering.api.wires.IImmersiveConnectable;
 import blusunrize.immersiveengineering.api.wires.LocalWireNetwork;
-import com.parvus.morewires.MoreImmersiveWires;
+import ca.teamdman.sfm.common.cablenetwork.CableNetwork;
+import ca.teamdman.sfm.common.cablenetwork.CableNetworkManager;
+import ca.teamdman.sfm.common.cablenetwork.ICableBlock;
 import com.parvus.morewires.network.NodeNetworkHandler;
-import net.minecraft.world.level.Level;
 
-public class SFMNetworkHandler extends NodeNetworkHandler<ConnectionWrapper, IGridNode> implements IGridNodeListener<com.parvus.morewires.compat.ae.AENetworkHandler> {
+public class SFMNetworkHandler extends NodeNetworkHandler<CableNetwork, CableNetworkManager>  {
 
 	protected SFMNetworkHandler(LocalWireNetwork net, GlobalWireNetwork global) {
 		super(net, global);
 	}
 
 	@Override
-	public void onSaveChanges(com.parvus.morewires.compat.sfm.SFMNetworkHandler var1, IGridNode var2) {
-
+	protected void clearConnection(CableNetwork cableNetwork) {
+		CableNetworkManager.unregisterNetworkForTestingPurposes(cableNetwork);
 	}
 
 	@Override
-	protected void clearConnection(ConnectionWrapper c) {
-		if(c.getConnection() != null)c.getConnection().destroy();
-	}
-
-	@Override
-	protected void initNode(Level level) {
-		mainNode.create(level, null);
-	}
-
-	@Override
-	protected IGridNode getNode() {
-		return mainNode.getNode();
-	}
-
-	@Override
-	protected ConnectionWrapper connect(IImmersiveConnectable iic, IGridNode nodeIn) {
-		if(iic instanceof AENetworkedBlockEntity te) {
-			IGridNode node = te.getActionableNode();
-			if(node != null) {
-				return new ConnectionWrapper(GridHelper.createConnection(nodeIn, node));
-			}
-			needRefresh = true;
-		}
+	protected CableNetworkManager getNode() {
 		return null;
 	}
 
 	@Override
-	protected void connectFirst(IImmersiveConnectable iic, IGridNode main) {
-		if(iic instanceof AENetworkedBlockEntity te) {
-			IGridNode node = te.getActionableNode();
-			if(node != null) {
-				mainNode.setOwningPlayerId(node.getOwningPlayerId());
-			}
-		}
+	protected CableNetwork connect(IImmersiveConnectable iic, CableNetworkManager node) {
+		return null;
 	}
+
+	@Override
+	protected void connectFirst(IImmersiveConnectable iic, CableNetworkManager main) {
+
+	}
+
+
 }
